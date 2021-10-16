6ddc227734fd02cd4b8d4b284afbcab5ec809aa8
root <bjpaul99@yahoo.com>
2021-10-16 19:06:32 +0000

    Added API for quotes

--- comments.js
+++ comments.js
@@ -6,3 +6,23 @@ function addComments() {
         document.getElementById("comment1").innerHTML = "<p>Tessa Perkins: That's cool!<p>";
     })
 }
+
+function addQuote() {
+
+    document.getElementById("quoteButton").addEventListener("click", event => {
+        event.preventDefault();
+
+        let url = "https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand";
+
+        fetch(url)
+            .then(response => {
+                return response.json()
+            }).then(json => {
+                console.log(json)
+                let i = Math.floor(Math.random() * 10) 
+                document.getElementById("quote").innerHTML = json[i].content.rendered
+                json = []
+            })
+    })
+
+}
