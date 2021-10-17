ad8771bbd10786f189db4af22b59f0f3f97dd774
root <bjpaul99@yahoo.com>
2021-10-17 00:45:11 +0000

    API and comments

--- javascript/comments.js
+++ javascript/comments.js
@@ -1,8 +1,47 @@
+//FIXME Making you have to click twice to show and hide comments
 function addComments() {
 
-    document.getElementById("button1").addEventListener("click", event => {
-        event.preventDefault()
+    if (document.getElementById("button1").innerHTML === "Show Comments") {
+        document.getElementById("button1").addEventListener("click", event => {
+            event.preventDefault()
 
-        document.getElementById("comment1").innerHTML = "<p>Tessa Perkins: That's cool!<p>";
+            document.getElementById("comment1").innerHTML = "<p><strong>Tessa Perkins:</strong> That's cool!<p>";
+
+            document.getElementById("button1").innerHTML = "Hide Comments";
+
+        })
+    }  
+
+    else {
+        document.getElementById("button1").addEventListener("click", event => {
+            event.preventDefault()
+
+            document.getElementById("comment1").innerHTML = "";
+
+            document.getElementById("button1").innerHTML = "Show Comments";
+
+        })
+    }
+}
+
+function addQuote() {
+
+    document.getElementById("quoteButton").addEventListener("click", event => {
+        event.preventDefault();
+
+        let url = "https://quotesondesign.com/wp-json/wp/v2/posts/"//?orderby=rand";
+
+        fetch(url)
+            .then(response => {
+                return response.json()
+            }).then(json => {
+                console.log(json)
+                let i = Math.floor(Math.random() * 10) 
+                document.getElementById("quote").innerHTML = json[i].content.rendered
+                for (let i = 0; i < json.length; ++i) {
+                    json[i] = null;
+                }
+            })
     })
+
 }
