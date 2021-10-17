08e03b7a3eb43053ccfdc33ed3224fbac465e618
Kade Greenburg <kadegreenburg@gmail.com>
2021-10-16 18:46:15 -0600

    minor updates

--- comments.js
+++
@@ -1,8 +0,0 @@
-function addComments() {
-
-    document.getElementById("button1").addEventListener("click", event => {
-        event.preventDefault()
-
-        document.getElementById("comment1").innerHTML = "<p>Tessa Perkins: That's cool!<p>";
-    })
-}
