34c785c0f6c7402ea66ee5c0c9bdfd6b9a6295ae
Kade Greenburg <kadegreenburg@gmail.com>
2021-10-16 18:46:39 -0600

    better comments

--- javascript/comments.js
+++ javascript/comments.js
@@ -1,8 +1,18 @@
+var comments = [];
+
+var showComments = true;
 function addComments() {
 
     document.getElementById("button1").addEventListener("click", event => {
         event.preventDefault()
-
-        document.getElementById("comment1").innerHTML = "<p>Tessa Perkins: That's cool!<p>";
+        if(showComments) {
+            showComments = false;
+            document.getElementById("comment1").innerHTML = "<p>Tessa Perkins: That's cool!<p>";
+        }
+        else {
+            showComments = true;
+            document.getElementById("comment1").innerHTML = "";
+        }
+        
     })
 }
