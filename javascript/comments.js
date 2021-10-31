7cefc59757f188d38cef592497bfd5248c0f638f
root <bjpaul99@yahoo.com>
2021-10-31 16:35:59 +0000

    Fixed comments button

--- javascript/comments.js
+++ javascript/comments.js
@@ -1,25 +1,25 @@
 function addComments() {
 
     if (document.getElementById("button1").innerHTML === "Show Comments") {
-        document.getElementById("button1").addEventListener("click", event => {
-            event.preventDefault()
+        //document.getElementById("button1").addEventListener("click", event => {
+            //event.preventDefault()
 
             document.getElementById("comment1").innerHTML = "<p><strong>Tessa Perkins:</strong> That's cool!<p>";
 
             document.getElementById("button1").innerHTML = "Hide Comments";
 
-        })
+        //})
     }  
 
     else {
-        document.getElementById("button1").addEventListener("click", event => {
-            event.preventDefault()
+        //document.getElementById("button1").addEventListener("click", event => {
+            //event.preventDefault()
 
             document.getElementById("comment1").innerHTML = "";
 
             document.getElementById("button1").innerHTML = "Show Comments";
 
-        })
+        //})
     }
 }
 
